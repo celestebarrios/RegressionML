a05c4468d2f6e04713fd15003870b5be7184fffe
celestebarrios <49879625+celestebarrios@users.noreply.github.com>
2020-10-01 12:50:21 -0500

    fourth

--- tensor/Regression.py
+++ tensor/Regression.py
@@ -4,6 +4,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 #Loading in Our Data
 data = pd.read_csv("C:/Users/celes/Documents/MachineLearning/tensor/student-mat.csv", sep = ";")
 print(data.head(5))
@@ -16,3 +19,13 @@ X = np.array(data.drop([predict],1))
 y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+accuracy = linear.score(x_test, y_test)
+print(accuracy)
+
+print('Coefficient: \n', linear.coef_)
+print('Coefficient: \n', linear.intercept_)
+
